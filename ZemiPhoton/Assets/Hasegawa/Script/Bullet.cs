a29fd35954450f57bebec6b2c553c5c5be3616c0
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-06-26 15:12:21 +0900

    Player関係の行動

--- ZemiPhoton/Assets/Hasegawa/Script/Bullet.cs
+++ ZemiPhoton/Assets/Hasegawa/Script/Bullet.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 	float speed = 1;
 	Vector3 angle;
+	Vector3 movement;
 	// Use this for initialization
 	void Start () {
+		// 角度の保持
 		angle = transform.localEulerAngles;
+		// 移動量計算
+		movement = new Vector3(
+			Mathf.Sin (angle.y * 3.14f / 180) * speed, 
+			-(Mathf.Tan (angle.x * 3.14f / 180) * speed), 
+			Mathf.Cos (angle.y * 3.14f / 180) * speed);
+		// 削除処理
+		Destroy (this.gameObject, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += new Vector3 (
-			Mathf.Sin (angle.y * 3.14f / 180) * speed, 
-			-(Mathf.Tan (angle.x * 3.14f / 180) * speed), 
-			Mathf.Cos (angle.y * 3.14f / 180) * speed);
+		// 移動
+		transform.position += movement;
 	}
 }
--- ZemiPhoton/Assets/Hasegawa/Script/Player.cs
+++ ZemiPhoton/Assets/Hasegawa/Script/Player.cs
@@ -13,15 +13,20 @@ public class Player : MonoBehaviour {
 	GameObject myBullet;
 	[SerializeField]
 	GameObject muzzle;
-
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// 視線移動
 		Eye ();
+		// 走る
+		if (Input.GetKey (KeyCode.LeftShift))
+			speed = 0.2f;
+		else
+			speed = 0.1f;
+		// 移動
 		if (Input.GetKey (KeyCode.A)) {
 			angle = 270;
 			move = true;
@@ -40,11 +45,16 @@ public class Player : MonoBehaviour {
 			move = true;
 		}
 		Move ();
+		// ショット
 		if (Input.GetMouseButton (0)) {
 			Shot ();
 		}
+		// ジャンプ
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump ();
+		}
 	}
-
+	// 移動
 	void Move(){
 		if (move) {
 			transform.position += new Vector3 (
@@ -54,14 +64,29 @@ public class Player : MonoBehaviour {
 			move = false;
 		}
 	}
-
+	// 視線移動
 	void Eye(){
 		mouseAngle += new Vector3 ((-(Input.GetAxis ("Mouse Y"))), (Input.GetAxis ("Mouse X")), 0);
 		mainAngle = Quaternion.Euler (mouseAngle);
 		transform.localRotation = mainAngle;
 	}
-
+	// 撃つ
 	void Shot(){
 		Instantiate (myBullet, muzzle.transform.position, transform.localRotation);
+	}
+	// ジャンプ
+	void Jump(){
+		StartCoroutine (Jumping ());
+	}
+	// ジャンプの中身
+	IEnumerator Jumping(){
+		Vector3 move = new Vector3 (0, 0.1f, 0);
+		while (true) {
+			transform.position += move;
+			move.y -= 0.001f;
+			if (move.y <= 0)
+				break;
+			yield return new WaitForSeconds (0.01f);
+		}
 	}
 }
