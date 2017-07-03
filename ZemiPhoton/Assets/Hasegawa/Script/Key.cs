4431ba371c1d9bf81aa6a586d7c8c64c7cf788e5
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-07-03 09:02:44 +0900

    キャラクタの走るモーションだけを取り付ける

---
+++ ZemiPhoton/Assets/Hasegawa/Script/Key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+	public const byte
+	NONE=0,UP=1,RIGHT=2,DOWN=4,LEFT=8,UPRIGHT=3,DOWNRIGHT=6,UPLEFT=9,DOWNLEFT=12,JUMP=24;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- ZemiPhoton/Assets/Hasegawa/Script/Player.cs
+++ ZemiPhoton/Assets/Hasegawa/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	float angle;
 	float speed = 0.1f;
+	byte type = 0;
 	bool move = false;
 	Vector3 mouseAngle = Vector3.zero;
 	Quaternion mainAngle;
@@ -12,7 +13,16 @@ public class Player : MonoBehaviour {
 	[SerializeField]
 	GameObject myBullet;
 	[SerializeField]
-	GameObject muzzle;
+	Transform muzzle;
+	[SerializeField]
+	Transform Head;
+	[SerializeField]
+	Transform myCollection;
+	[SerializeField]
+	Animator animator;
+
+	float motion = 0;
+
 	// Use this for initialization
 	void Start () {
 	}
@@ -21,30 +31,7 @@ public class Player : MonoBehaviour {
 	void Update () {
 		// 視線移動
 		Eye ();
-		// 走る
-		if (Input.GetKey (KeyCode.LeftShift))
-			speed = 0.2f;
-		else
-			speed = 0.1f;
-		// 移動
-		if (Input.GetKey (KeyCode.A)) {
-			angle = 270;
-			move = true;
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			angle = 90;
-			move = true;
-		}
-		Move ();
-		if (Input.GetKey (KeyCode.W)) {
-			angle = 0;
-			move = true;
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			angle = 180;
-			move = true;
-		}
-		Move ();
+		KeyMove ();
 		// ショット
 		if (Input.GetMouseButton (0)) {
 			Shot ();
@@ -53,26 +40,25 @@ public class Player : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			Jump ();
 		}
+
+		UnityChanAnimation ();
 	}
-	// 移動
-	void Move(){
-		if (move) {
-			transform.position += new Vector3 (
-				Mathf.Sin ((transform.localEulerAngles.y + angle) * 3.14f / 180) * speed, 
-				0, 
-				Mathf.Cos ((transform.localEulerAngles.y + angle) * 3.14f / 180) * speed);
-			move = false;
-		}
-	}
+
 	// 視線移動
 	void Eye(){
-		mouseAngle += new Vector3 ((-(Input.GetAxis ("Mouse Y"))), (Input.GetAxis ("Mouse X")), 0);
+		// マウス移動量を保存
+		mouseAngle += new Vector3 (-(Input.GetAxis ("Mouse Y")), (Input.GetAxis ("Mouse X")), 0);
+		if (mouseAngle.x <= -10)
+			mouseAngle.x = -10;
+		// 角度に変換
 		mainAngle = Quaternion.Euler (mouseAngle);
-		transform.localRotation = mainAngle;
+
+		transform.localRotation = new Quaternion (0, mainAngle.y, 0, mainAngle.w);
+		myCollection.localRotation = new Quaternion (mainAngle.x, myCollection.localRotation.y, myCollection.localRotation.z, myCollection.localRotation.w);
 	}
 	// 撃つ
 	void Shot(){
-		Instantiate (myBullet, muzzle.transform.position, transform.localRotation);
+		Instantiate (myBullet, muzzle.position, transform.localRotation);
 	}
 	// ジャンプ
 	void Jump(){
@@ -89,4 +75,83 @@ public class Player : MonoBehaviour {
 			yield return new WaitForSeconds (0.01f);
 		}
 	}
+	// Unityちゃんモーション
+	void UnityChanAnimation(){
+		animator.SetFloat ("Speed", motion);
+	}
+
+	// キー移動判定
+	void KeyMove(){
+		type = Key.NONE;
+		// 走る
+		if (Input.GetKey (KeyCode.LeftShift))
+			speed = 0.2f;
+		else
+			speed = 0.1f;
+		// 移動
+		if (Input.GetKey (KeyCode.A)) {
+			type += Key.LEFT;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			type += Key.RIGHT;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			type += Key.UP;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			type += Key.DOWN;
+		}
+
+		switch (type) {
+		case Key.UP:
+			angle = 0;
+			break;
+		case Key.DOWN:
+			angle = 180;
+			break;
+		case Key.RIGHT:
+			angle = 90;
+			break;
+		case Key.LEFT:
+			angle = 270;
+			break;
+		case Key.UPLEFT:
+			angle = 315;
+			break;
+		case Key.UPRIGHT:
+			angle = 45;
+			break;
+		case Key.DOWNLEFT:
+			angle = 225;
+			break;
+		case Key.DOWNRIGHT:
+			angle = 135;
+			break;
+		case Key.NONE:
+			break;
+		default:
+			Debug.Log ("Error :: Player move type");
+			break;
+		}
+		if (type != 0)
+			move = true;
+
+		Move ();
+	}
+
+	// 移動
+	void Move(){
+		Vector3 movement = Vector3.zero;
+		if (move) {
+			movement = new Vector3 (
+				Mathf.Sin ((transform.localEulerAngles.y + angle) * 3.14f / 180) * speed, 
+				0, 
+				Mathf.Cos ((transform.localEulerAngles.y + angle) * 3.14f / 180) * speed);
+			transform.position += movement;
+			move = false;
+			motion = 1;
+		} else
+			motion = 0;
+	}
+
 }
