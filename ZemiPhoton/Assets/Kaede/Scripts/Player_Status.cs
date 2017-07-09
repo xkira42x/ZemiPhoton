80f87783707e377d9d6fc89f73a5927233c2acb6
DA\nwuser <xkira42x@gmail.com>
2017-07-09 20:13:40 +0900

    かえでっちゃんのスクリプト多少いじり

--- ZemiPhoton/Assets/Kaede/Scripts/Player_Status.cs
+++ ZemiPhoton/Assets/Kaede/Scripts/Player_Status.cs
@@ -8,21 +8,21 @@ public class Player_Status : Photon.MonoBehaviour {
 	private int userid;
 	private int hp = 100;
 
-	private int ouserid;
-	private int ohp;
+	public int ouserid;
+	public int ohp;
 
-	private int[,] supporter = new int[4,2];
+	public int[,] supporter = new int[4,2];
 
 
 	void Start(){
 
+		for (int i = 0; i < 4; i++) {
+			supporter [i, 0] = -1;
+			supporter [i, 1] = -1;
+		}
 		if (photonView.isMine) {
 			userid = PhotonNetwork.player.ID;
 
-			for (int i = 0; i < 4; i++) {
-				supporter [i, 0] = -1;
-				supporter [i, 1] = -1;
-			}
 		}
 
 	}
@@ -30,7 +30,7 @@ public class Player_Status : Photon.MonoBehaviour {
 	void Update(){
 
 		if (photonView.isMine) {
-/*			switch (userid) {
+			switch (userid) {
 			case 1:
 				for (int i = 1; i < 4; i++) {
 					if (supporter [i, 0] != -1) {
@@ -42,17 +42,18 @@ public class Player_Status : Photon.MonoBehaviour {
 				break;
 			case 2:
 				for (int i = 0; i < 4; i++) {
-					if (supporter [i, 0] != -1) {
-						if (i == 0) {
+					//0,1,2,3
+					if (i != 1) {
+						//0,2,3
+						if (supporter [i, 0] != -1) {
+							//0
 							GameObject.Find ("Suppoter" + (i + 1)).GetComponent<Text> ().text = "ユーザ" + supporter [i, 0] + ":" + supporter [i, 1];
-						} else if (i > 1) {
-							GameObject.Find ("Suppoter" + i).GetComponent<Text> ().text = "ユーザ" + supporter [i, 0] + ":" + supporter [i, 1];
-						} 
-					} else {
-						GameObject.Find ("Suppoter" + i).GetComponent<Text> ().text = "";
+						} else {
+							//2,3
+							GameObject.Find ("Suppoter" + i).GetComponent<Text> ().text = "";
+						}
 					}
 				}
-				
 				break;
 			case 3:
 				for (int i = 0; i < 4; i++) {
@@ -81,34 +82,48 @@ public class Player_Status : Photon.MonoBehaviour {
 			default:
 				break;
 			}
-*/
+
 		}
 
-		if (Input.GetKey (KeyCode.A)) {
-			for (int j = 0; j < 4; j++) {
-				Debug.Log (supporter [j, 0]);
+		if (Input.GetKeyUp (KeyCode.A)) {
+			if (photonView.isMine) {
+				for (int j = 0; j < 4; j++) {
+					Debug.Log ("on"+supporter [j, 0]);
+				}
+			} else {
+				for (int j = 0; j < 4; j++) {
+					Debug.Log ("not"+supporter [j, 0]);
+				}
 			}
 		}
+			
 
-		Debug.Log ("receive");
-		Debug.Log (supporter [3,0]);
+//		Debug.Log ("receive");
+//		Debug.Log (supporter [3,0]);
 
 
 	}
 
 	void OnPhotonSerializeView(PhotonStream stream,PhotonMessageInfo info){
-		if (stream.isWriting) {
-			stream.SendNext (userid);
-			stream.SendNext (hp);
-		} else {
-			ouserid = (int)stream.ReceiveNext ();
-			ohp = (int)stream.ReceiveNext ();
+		if (photonView.isMine) {
+			if (stream.isWriting) {
+				stream.SendNext (userid);
+				stream.SendNext (hp);
+			} else {
+				ouserid = (int)stream.ReceiveNext ();
+				ohp = (int)stream.ReceiveNext ();
 
-			for (int i = 0; i < 4; i++) {
+
+				supporter [ouserid - 1, 0] = ouserid;
+				supporter [ouserid - 1, 1] = ohp;
+				/*			for (int i = 0; i < 4; i++) {
+				Debug.Log ("ouserid:" + ouserid);
+				Debug.Log ("i:" + i);
 				if (ouserid == i + 1) {
 					supporter [i, 0] = ouserid;
 					supporter [i, 1] = ohp;
 				}
+			}*/
 			}
 		}
 	}
