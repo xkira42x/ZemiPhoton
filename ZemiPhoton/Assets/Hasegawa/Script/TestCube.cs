e9672541cad90f3ad4f2f70e40d399b7be013540
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-07-14 21:09:17 +0900

    LOD試作品

---
+++ ZemiPhoton/Assets/Hasegawa/Script/TestCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCube : MonoBehaviour {
+
+	[SerializeField]
+	Transform trans;
+	[SerializeField]
+	float f = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = trans.position + Vector3.down * f;
+	}
+}
---
+++ ZemiPhoton/Assets/Hasegawa/Script/move_test.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_test : MonoBehaviour {
+
+	// 移動速度
+	float S_Speed = 0.1f;
+	// 移動方向
+	byte S_Type = 0;
+
+	const byte NONE=0,UP=1,DOWN=2;
+	byte S_Jtype = NONE;
+	float JumpGravity;
+
+	[SerializeField]
+	bool isGround;
+	void IsGround(){isGround = Physics.Raycast (transform.position, Vector3.down, 1.6f);}
+
+	void Start(){
+		
+	}
+
+	void Update(){
+			MyMain ();
+	}
+
+	// キー移動判定
+	void S_KeyMove(){
+		S_Type = Key.NONE;
+		// 走る
+		if (Input.GetKey (KeyCode.LeftShift))
+			S_Speed = 0.15f;
+		else
+			S_Speed = 0.1f;
+		// キー判定
+		if (Input.GetKey (KeyCode.W)) {
+			S_Type += Key.FORWARD;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			S_Type += Key.BACK;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			S_Type += Key.LEFT;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			S_Type += Key.RIGHT;
+		}
+		S_Move ();
+	}
+
+	// 移動
+	void S_Move(){
+		Vector3 pos = Vector3.zero;
+		// 移動
+		switch (S_Type) {
+		case Key.FORWARD:
+			pos += transform.forward * S_Speed;
+			break;
+		case Key.BACK:
+			pos += -(transform.forward * S_Speed);
+			break;
+		case Key.RIGHT:
+			pos += transform.right * S_Speed;
+			break;
+		case Key.LEFT:
+			pos += -(transform.right * S_Speed);
+			break;
+		case Key.FORWARDLEFT:
+			pos += transform.forward * S_Speed;
+			pos += -(transform.right * S_Speed);
+			break;
+		case Key.FORWARDRIGHT:
+			pos += transform.forward * S_Speed;
+			pos += transform.right * S_Speed;
+			break;
+		case Key.BACKLEFT:
+			pos += -(transform.forward * S_Speed);
+			pos += -(transform.right * S_Speed);
+			break;
+		case Key.BACKRIGHT:
+			pos += -(transform.forward * S_Speed);
+			pos += transform.right * S_Speed;
+			break;
+		case Key.NONE:
+			break;
+		default:
+			Debug.Log ("Error :: Player move S_Type");
+			break;
+		}
+		transform.localPosition += pos;
+	}
+
+	void S_Jump(){
+		if (isGround && Input.GetKeyDown (KeyCode.Space)) {
+			S_Jtype = UP;
+			JumpGravity = 0.1f;
+		}
+		switch (S_Jtype) {
+		case UP:
+			S_ToJump ();
+			break;
+		case DOWN:
+			S_DropDown ();
+			break;
+		}
+	}
+
+	void S_ToJump(){
+		transform.position += Vector3.up * JumpGravity;
+		JumpGravity *= 0.98f;
+		if (JumpGravity <= 0.07f)
+			S_Jtype = DOWN;
+	}
+
+	void S_DropDown(){
+		if (!isGround)
+			transform.position += Vector3.down * 0.1f;
+	}
+
+	// メイン処理
+	void MyMain(){
+
+		// キー移動
+		S_KeyMove ();
+
+		// ジャンプ
+		S_Jump();
+
+		IsGround ();
+	}
+}
