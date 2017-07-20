2f1f9c2798f132308c59d067a35d486d3ebe2416
DA\nwuser <xkira42x@gmail.com>
2017-07-21 00:05:16 +0900

    エスケープキーで終了UIを表示,ｙ座標の固定するスクリプトの追加

---
+++ ZemiPhoton/Assets/Ik/Scripts/N3_EndKey.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//ゲームを強制終了させるキーです
+public class N3_EndKey : MonoBehaviour {
+
+	public GameObject EndUI;
+
+	GameObject gObj;
+	// Update is called once per frame
+	void Update () {
+		//Escapeキーでゲーム終了ボタンを表示
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			gObj = Instantiate (EndUI);
+			gObj.transform.name = EndUI.name;
+			gObj.transform.parent=GameObject.Find ("Canvas").transform;
+			ShowMouse (false);
+		}
+	}
+	public void EndButton(){
+		Application.Quit ();
+	}
+	public void ContnButton(){
+		ShowMouse (true);
+		Destroy (GameObject.Find(EndUI.name));
+	}
+	public void ShowMouse(bool flg){
+		Screen.lockCursor = flg;
+	}
+}
---
+++ ZemiPhoton/Assets/Ik/Scripts/N3_Yfreez.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Ｙ座標を固定させてます
+public class N3_Yfreez : MonoBehaviour {
+
+	Vector3 FreezePos=Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		FreezePos.y = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (this.transform.position.x,
+			FreezePos.y,
+			this.transform.position.z);
+	}
+}
