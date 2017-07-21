e5780a284cae50de3a1aa8d886238f864dd1e98c
unknown <nwuser@GM2150047.da.iwasaki.ac.jp>
2017-07-21 15:19:35 +0900

    ogi

--- ZemiPhoton/Assets/Ogiyama/NewData.cs
+++ ZemiPhoton/Assets/Ogiyama/NewData.cs
@@ -7,14 +7,43 @@ public class NewData : MonoBehaviour {
 
 	public Text UserMessage;
 	public Text UserName;
-	private int Button_flg=0;
+	public Text Button_Message;
+	public int Button_flg=0;
 
 	public string ServerAddress = "10.22.1.79/3zemi/DB_test_unity_input.php";
 
-	public void CreateUser_Button_Push(){
-		UserMessage.GetComponent<Text> ().text = "Prese New UserName&PassWord";
-		UserName.GetComponent<Text> ().text = "User Name";
+	public void NewData_Button_Push(){
+		if (Button_flg == 0) {
+			UserMessage.GetComponent<Text> ().text = "Prese New UserName&PassWord";
+			UserName.GetComponent<Text> ().text = "User Name";
+			Button_Message.GetComponent<Text> ().text = "Enter!";
+			Button_flg = 1;
+		} else {
+			UserMessage.GetComponent<Text> ().text = "Prease\nID&PassWord";
+			UserName.GetComponent<Text>().text="ID";
+			Button_Message.GetComponent<Text> ().text = "New Data";
+			Button_flg = 0;
 
+			StartCoroutine ("DataAccess");
+
+		}
+	}
+		
+	private IEnumerator DataAccess(){
+		Dictionary<string,string> dic = new Dictionary<string,string> ();
+
+		dic.Add ("name", UserName.GetComponent<Text> ().text);
+		dic.Add ("pass", Catch.InputPass, GetComponent<Text> ().text);
+
+		StartCoroutine(DataPost(ServerAddress,dic));
+		yield return 0;
+	}
+
+	private IEnumerator DataPost(string url,Dictionary<string,string>post){
+		WWWForm form = new WWWForm ();
+		foreach (KeyValuePair<string,string>post_arg in post) {
+			form.AddField (post_arg.Key, post_arg.Value);
+		}
 	}
 
 }
