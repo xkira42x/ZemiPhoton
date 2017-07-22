13c882ce875e6038d7864995dbf50ed454272bb9
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-07-23 02:13:13 +0900

    マズルフラッシュの試作

--- ZemiPhoton/Assets/Hasegawa/Script/S3_Shot.cs
+++ ZemiPhoton/Assets/Hasegawa/Script/S3_Shot.cs
@@ -9,28 +9,53 @@ public class S3_Shot : Photon.MonoBehaviour {
 	Transform S_Muzzle;
 	[SerializeField]
 	Transform S_Collection;
-	// ショットフラグ
-	bool S_SHOOTING = false;
-	public bool S_Shooting{ get { return S_SHOOTING; } set { S_SHOOTING = value; } }
+	[SerializeField]
+	GameObject MuzzleFlash;
+	ParticleSystem[] MuzzleFlashEffect = new ParticleSystem[2];
 
-	
+	// インターバル
+	float interval = 0.1f;
+	public float Interval{ get { return interval; } set { interval = value; } }
+	// ショットフラグ
+	bool S_shoot = false;
+	bool loading = false;
+	public bool S_Shoot{ get { return S_shoot; } set { S_shoot = value; } }
+
+	void Start(){
+		MuzzleFlashEffect = MuzzleFlash.GetComponentsInChildren<ParticleSystem> ();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		// プレイヤーコントロール設定
 		if (photonView.isMine) {
 			// ショット
-			if (Input.GetMouseButton (0))
-				S_SHOOTING = true;
-			else
-				S_SHOOTING = false;
+			if (Input.GetMouseButton (0) && !loading) {
+				S_shoot = true;
+				loading = true;
+				StartCoroutine (NextLoading ());
+			} else
+				S_shoot = false;
 		}
 
-		if (S_SHOOTING) {
+		if (S_shoot) {
 			S_Shot ();
+		} else {
+			for (int i = 0; i < MuzzleFlashEffect.Length; i++) {
+				MuzzleFlashEffect [i].Stop ();
+			}
 		}
 	}
 	// 撃つ
 	void S_Shot(){
+		for (int i = 0; i < MuzzleFlashEffect.Length; i++) {
+			MuzzleFlashEffect [i].Play ();
+		}
 		Instantiate (S_Bullet, S_Muzzle.position, transform.localRotation * S_Collection.localRotation);
+	}
+	// 装填中
+	IEnumerator NextLoading(){
+		yield return new WaitForSeconds (interval);
+		loading = false;
 	}
 }
