d16327979c9aa4ce513b876e46d2e91e64e58fb3
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-09-21 02:00:38 +0900

    S1_Moveから同期処理を切り離す、Photon.Behaviorなしで動かせる形にする 切り離した同期処理も、単体で動くように修正を加えた

--- ZemiPhoton/Assets/Resources/Hasegawa/Script/Key.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Key : MonoBehaviour {
-
-	public const byte
-	NONE=0,FORWARD=1,RIGHT=2,BACK=4,LEFT=8,FORWARDRIGHT=3,BACKRIGHT=6,FORWARDLEFT=9,BACKLEFT=12,JUMP=24;
-}
