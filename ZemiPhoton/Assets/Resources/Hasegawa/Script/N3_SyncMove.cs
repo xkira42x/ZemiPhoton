43ad196d04815e017aa487f9ddc1c74a4acd23c6
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-09-21 02:00:38 +0900

    S1_Moveから同期処理を切り離す、Photon.Behaviorなしで動かせる形にする 切り離した同期処理も、単体で動くように修正を加えた

--- ZemiPhoton/Assets/Resources/Hasegawa/Script/N3_SyncMove.cs
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/N3_SyncMove.cs
@@ -1,43 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Runtime.InteropServices;
+//using System;
+//using System.Runtime.InteropServices;
 
 public class N3_SyncMove : Photon.MonoBehaviour {
 
-	//PhotonView N_photonView;
-	Vector3 N_syncPos = Vector3.zero;
-	//Vector3 N_nowPos = Vector3.zero;
-	public Vector3 GetSyncPos(){return N_syncPos;}
+	[SerializeField]S1_Move S_Move;
 
-	public Vector3 pos;
-	bool isJump = false;
-	public bool IsJump{ get { return isJump; } set { isJump = value; } }
-
-	void Awake(){
-		//初期生成時にも同期が起きてしまうため、前回の座標を生成時の座標へ
-		//N_nowPos = transform.position;
-	}
-
-	// Use this for initialization
-	void Start () {
-		//ネットワーク
-		PhotonNetwork.NetworkStatisticsEnabled = true;
-		//N_photonView = PhotonView.Get(this);
-	}
-
-	void OnPhotonSerializeView(PhotonStream stream,PhotonMessageInfo info){
-		if (stream.isWriting) {
-			//座標の差分値を送信
-			//stream.SendNext (transform.position - N_nowPos);
-			//N_nowPos = transform.position;
-			stream.SendNext(transform.position);
-			stream.SendNext(isJump);
-		} else {
-			//データの受信
-			N_syncPos = (Vector3)stream.ReceiveNext ();
-			isJump = (bool)stream.ReceiveNext ();
+	void Update(){
+		if (photonView.isMine) {
+			photonView.RPC ("SyncPosition", PhotonTargets.Others, transform.position);
 		}
+	}
+
+	[PunRPC]
+	void SyncPosition(Vector3 pos){
+		transform.position = pos;
 	}
 }
--- ZemiPhoton/Assets/Resources/Hasegawa/Script/S1_Move.cs
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/S1_Move.cs
@@ -1,22 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 using System.Runtime.InteropServices;
 
-public class S1_Move : Photon.MonoBehaviour {
-	// 同期スクリプト参照
-	N3_SyncMove N_syncMove;
-	// 同期座標
-	Vector3 N_SyncPos;
+public class S1_Move : MonoBehaviour {
+
 	// 移動速度
-	float S_Speed;
-	public float Speed{ get { return S_Speed; } set { S_Speed = value; } }
-	// 移動方向
-	byte S_Type = 0;
-	[SerializeField] Transform myTransform;
-	//[SerializeField] Animator S_Animator;
-	//[SerializeField]S4_Animation S_Animation;
-	float S_Motion = 0;
+	[SerializeField]float speed = 0.1f;
+
+	float _motion = 0;
+	public float motion{get{ return _motion;}}
 
 	// ジャンプ判定群
 	const byte NONE=0,UP=1,DOWN=2;
@@ -29,74 +23,27 @@ public class S1_Move : Photon.MonoBehaviour {
 	[SerializeField] bool isGround;
 	void IsGround(){isGround = Physics.Raycast (transform.position, Vector3.down, 0.3f);}
 
-	//IK追記
-	[SerializeField] bool mineflg;
 
 	void Start(){
-		N_SyncPos = transform.position;
-		if (!photonView.isMine)
-			N_syncMove = GetComponent<N3_SyncMove> ();
 	}
 
 	void Update(){
-		if (photonView.isMine||mineflg==true) {
-			MyMain ();
-		} else {
-			// 同期処理の呼び出し
-			SyncPosition();
-		}
+		// キー移動
+		S_KeyMove ();
+
+		// ジャンプ
+		//S_Jump();
+
+		IsGround ();
+
 	}
-
-
-	// Unityちゃんモーション
-	void S_UnityChanAnimation(){
-		//S_Animator.SetFloat ("Speed", S_Motion);
-		//S_Animator.SetBool ("IsJumping",isJumping);
-	}
-
+		
 	// キー移動判定
 	void S_KeyMove(){
-		S_Type = Key.NONE;
-		// キー判定
-		if (Input.GetKey (KeyCode.W)) {
-			S_Type += Key.FORWARD;
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			S_Type += Key.BACK;
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			S_Type += Key.LEFT;
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			S_Type += Key.RIGHT;
-		}
-		S_Move ();
-	}
-
-	// 移動
-	void S_Move(){
-		int angle = 0;
-		// 移動
-		switch (S_Type) {
-		case Key.FORWARD		:angle=0;	break;
-		case Key.BACK			:angle=180;	break;
-		case Key.RIGHT			:angle=90;	break;
-		case Key.LEFT			:angle=270;	break;
-		case Key.FORWARDLEFT	:angle=315;	break;
-		case Key.FORWARDRIGHT	:angle=45;	break;
-		case Key.BACKLEFT		:angle=215;	break;
-		case Key.BACKRIGHT		:angle=135;	break;
-		case Key.NONE:break;
-		default:Debug.Log ("Error :: Player move S_Type");break;
-		}
-		if (S_Type != NONE) {
-			transform.position += new Vector3 (
-				Mathf.Sin ((myTransform.localEulerAngles.y + transform.localEulerAngles.x + angle) * 3.14f / 180) * 0.1f, 
-				0, 
-				Mathf.Cos ((myTransform.localEulerAngles.y  + angle) * 3.14f / 180) * 0.1f);
-		}
-		// モーション更新
-		S_Speed = (S_Type != Key.NONE)? 1 : 0;
+		float horizontal = CrossPlatformInputManager.GetAxis ("Horizontal") * speed;
+		float vertical = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
+		transform.Translate (horizontal, 0, vertical);
+		_motion = (horizontal != 0 || vertical != 0) ? 1 : 0;
 	}
 
 	// ジャンプ
@@ -109,10 +56,10 @@ public class S1_Move : Photon.MonoBehaviour {
 		// 判定分岐
 		switch (S_Jtype) {
 		case UP:
-			S_ToJump ();
+			ToJump ();
 			break;
 		case DOWN:
-			S_DropDown ();
+			DropDown ();
 			break;
 		}
 	}
@@ -123,7 +70,7 @@ public class S1_Move : Photon.MonoBehaviour {
 		JumpGravity = 0.3f;
 	}
 	// 上昇処理
-	void S_ToJump(){
+	void ToJump(){
 		// 重力
 		AddGravity ();
 		// 頂点判定
@@ -131,7 +78,7 @@ public class S1_Move : Photon.MonoBehaviour {
 			S_Jtype = DOWN;
 	}
 	// 下降処理
-	void S_DropDown(){
+	void DropDown(){
 		// 床判定
 		if (!isGround)
 			// 重力
@@ -145,37 +92,5 @@ public class S1_Move : Photon.MonoBehaviour {
 	void AddGravity(){
 		transform.position += Vector3.up * JumpGravity;
 		JumpGravity -= 0.98f * Time.deltaTime;		
-	}
-
-	// メイン処理
-	void MyMain(){
-
-		// キー移動
-		S_KeyMove ();
-
-		// ジャンプ
-		//S_Jump();
-
-		IsGround ();
-
-		// アニメーション
-		S_UnityChanAnimation ();
-	}
-
-	// 座標同期
-	void SyncPosition(){
-		// 同期座標取得
-		N_SyncPos = N_syncMove.GetSyncPos ();
-		Vector3 movement = (N_SyncPos - transform.position) * 0.5f;
-		movement = new Vector3 (movement.x, 0, movement.z);
-		// 移動処理とアニメーション処理
-		if (movement != Vector3.zero) {
-			S_Motion = 1;
-			transform.position += movement;
-		} else
-			S_Motion = 0;
-		isJumping = N_syncMove.IsJump;
-		// アニメーション
-		S_UnityChanAnimation ();
 	}
 }
