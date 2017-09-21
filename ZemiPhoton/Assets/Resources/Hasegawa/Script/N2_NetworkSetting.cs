91706c834bcc475834a7672d76e9a562c179756e
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-09-21 13:34:54 +0900

    S2_AngleをPhotonBehaviorなしの状態に修正 N2_NetworkSettingで同期に関係のないモノを排除する処理を加える

--- ZemiPhoton/Assets/Resources/Hasegawa/Script/N2_NetworkSetting.cs
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/N2_NetworkSetting.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class N2_NetworkSetting : Photon.MonoBehaviour {
-	[SerializeField]
-	Camera myCamera;
-	[SerializeField]
-	GameObject Body;
+	[SerializeField]Camera myCamera;
+	[SerializeField]GameObject Body;
+	[SerializeField]S1_Move move;
+	[SerializeField]S2_Angle angle;
 
 	void Start () {
 		if (photonView.isMine) {
 			myCamera.gameObject.SetActive (true);
 			Body.SetActive (false);
+			move.enabled = true;
+			angle.enabled = true;
 		}
 	}
 }
