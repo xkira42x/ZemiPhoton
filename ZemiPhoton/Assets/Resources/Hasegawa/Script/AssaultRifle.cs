d661f9154c912e3754736539c1b6652d34453402
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-10-08 00:14:59 +0900

    ハンドガンの追加

---
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/AssaultRifle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssaultRifle : GunBase {
+
+	public override void Action ()
+	{
+		base.Action ();
+	}
+}
---
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/HandGun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGun : GunBase {
+
+	public override void Action ()
+	{
+		if (Input.GetMouseButtonDown(0)) {
+			base.Action ();
+		}
+	}
+
+
+}
