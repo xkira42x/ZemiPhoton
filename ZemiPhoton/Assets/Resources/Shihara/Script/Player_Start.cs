e834698744b116e29c896daa1a320e583a1a6780
Amakitsune <6mikagura.ten9@gmail.com>
2017-10-13 10:27:29 +0900

    ゲームスタート少し修正

--- ZemiPhoton/Assets/Resources/Shihara/Script/Player_Start.cs
+++ ZemiPhoton/Assets/Resources/Shihara/Script/Player_Start.cs
@@ -15,11 +15,12 @@ public class Player_Start : Photon.MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //ボタンに変更
+        //ボタン入力に変更////////////////////////////////
         if (Input.GetKeyDown(KeyCode.Space)) OK();
 
         if (Input.GetKeyDown(KeyCode.C))
         photonView.RPC("Check", PhotonTargets.All, ok);
+        //////////////////////////////////////////////////
 
     }
 
@@ -30,16 +31,16 @@ public class Player_Start : Photon.MonoBehaviour {
         for(int i = 0; i <= PhotonNetwork.countOfPlayers; i++)
         {
             if (Check_ok == true) check++;
-            if (check >= PhotonNetwork.countOfPlayers) check = PhotonNetwork.countOfPlayers;
-            check = 0;
+            //if (check >= PhotonNetwork.countOfPlayers) check = PhotonNetwork.countOfPlayers;
         }
 
         //二人以上でスタート
-        if (1 <= check && check == PhotonNetwork.countOfPlayers)
+        if (2 <= check && check == PhotonNetwork.countOfPlayers)
         {
             Debug.Log("start");
             start = true;
         }
+        check = 0;
     }
 
     public void OK()
