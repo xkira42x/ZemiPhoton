75a5858ec4d6e7e48702ef47d90eec21e66d88c1
nemotu <nasusensei0413@gmail.com>
2017-10-20 09:39:56 +0900

    ~10月20日~ 敵ゾンビのスクリプト更新 残り作業：モデルチェンジ、アニメーション、爆発エフェクト

--- ZemiPhoton/Assets/Resources/Nemoto/A_normal_enemy_move.cs
+++ ZemiPhoton/Assets/Resources/Nemoto/A_normal_enemy_move.cs
@@ -1,65 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 /// <summary>
 /// 敵行動スクリプト(機能→状態遷移、動き、攻撃、当たり判定)
 /// </summary>
 public class A_normal_enemy_move : Photon.MonoBehaviour {
 
-    protected float A_spd;             //>敵のスピード(とりあえず現在は適当に数値をIN)>>少ないデータ型(求
+    protected float A_spd;                     //>敵のスピード(とりあえず現在は適当に数値をIN)>>少ないデータ型(求
     protected float A_rad;                     //>ラジアン(プレイヤー追尾にて使用)
     protected short A_hp_init = 100;           //>体力初期値 (16bit)->扱える数値(-32768~32767)             
     protected short A_hp;                      //>体力
     protected short A_power = 10;              //>攻撃力
     protected float A_magnitude;               //>二点間の距離(プレイヤーとの距離)
-    protected float A_target_magnitude = 2f;    //>プレイヤーに対しての攻撃判定距離
+    protected float A_target_magnitude = 2f;   //>プレイヤーに対しての攻撃判定距離
     protected Animator A_anim;                 //>敵(ゾンビ)のアニメーター
-    protected bool A_delay_flg = true;            //>ダメージ処理の抑制flg(多段HIT防止)
+    protected bool A_delay_flg = true;         //>ダメージ処理の抑制flg(多段HIT防止)
+    protected GameObject A_Player;             //>プレイヤーオブジェクト
+    protected N2_status A_P_info;              //>プレイヤーの情報
+    public GameObject A_Bullet;                //>弾オブジェクト
+    protected Bullet A_B_info;                 //>弾の情報
 
-    protected GameObject A_Player;                //>プレイヤーオブジェクト
-    protected int A_player_target;                       //>どのプレイヤーをターゲットするか
-    protected N2_status A_P_info;                        //>プレイヤーの情報
-    public GameObject A_Bullet;
-    protected Bullet A_B_info;                           //>弾の情報
-    
 
     /// <summary>
     /// マップ外待機(アクティブfalse)、待機、視認、攻撃、hit、死亡
     /// </summary>
-    protected enum A_enemy_state
+    protected enum  A_enemy_state
     {/*****使わないstateは後に消す******/
-        A_Stand_by, //Stand-by    ->マップ外待機(アクティブfalse)
-        A_oov,      //Out-of-view ->待機
-        A_vsb,      //Visibility  ->視認(移動)
-        A_atk,      //attack      ->攻撃
-        A_hit,      //Hit         ->攻撃が当たった
-        A_death     //Death       ->死亡
+       // A_Stand_by, //>Stand-by    ->マップ外待機(アクティブfalse)
+        A_oov,      //>Out-of-view ->待機
+        A_vsb,      //>Visibility  ->視認(移動)
+        A_atk,      //>attack      ->攻撃
+        A_hit,      //>Hit         ->攻撃が当たった
+        A_death,    //>Death       ->死亡
     }protected A_enemy_state A_state;
+    
+
+    //↑の列挙の状態のアニメーション情報
+    protected string[] A_animator_state = { "play" , "run", "attack", "play", "dide" };//play2つの理由→ヒットモーションなし
+
 
     void Start()
     {
-        A_Player = GameObject.Find("Player1");
-
-        /*while (true) {
-			A_player_target = Random.Range (1, 3);
-			if (A_player_target == 1)
-				A_Player = GameObject.Find ("Player1");
-			else if (A_player_target == 2)
-				A_Player = GameObject.Find ("Player2");
-                
-			if (A_Player != null)
-				break;
-            }*/
-
+        A_Player_Select();                             //>プレイヤーをランダムで参照（狙う）
         A_anim = GetComponent<Animator>();
 		A_P_info = A_Player.GetComponent<N2_status>();
         A_B_info = A_Bullet.GetComponent<Bullet>();
-        A_anim.SetBool("play", true);
-        A_hp = A_hp_init;                               //>体力初期化
+        A_hp = A_hp_init;                              //>体力初期化
         A_spd = Random.Range(0.04f, 0.1f);
         A_state = A_enemy_state.A_vsb;
-        //StartCoroutine ("Target");
+        A_anim.SetBool("play", true);
     }
     // Update is called once per frame
     void Update()
@@ -71,29 +60,25 @@ public class A_normal_enemy_move : Photon.MonoBehaviour {
     /// <summary>
     /// 敵の動作関数→状態遷移も含む
     /// </summary>
-    /// <param name="A_spd"></param>
     protected void A_Enemy_Move()
     {
-
-		switch (A_state) {
-		case A_enemy_state.A_Stand_by://マップ外の待機状態(リセット)
+        A_anim.SetBool(A_animator_state[(int)A_state], true);//>状態毎のアニメーション
+        switch (A_state) {
+		/*case A_enemy_state.A_Stand_by://マップ外の待機状態(リセット)
 			A_hp = A_hp_init;
 			break;
-
+            */
 		case A_enemy_state.A_oov://待機状態
-                /*待機モーション*/
-			A_anim.SetBool ("play", true);
-                /***************/
+             
 			if (A_anim.GetCurrentAnimatorStateInfo (0).normalizedTime >= 0.15f) {
 
 				/*攻撃に切り替える*/
 				A_magnitude = (transform.position - A_Player.transform.position).sqrMagnitude;//>二点間の距離
 				if (A_magnitude <= A_target_magnitude) {
-					A_anim.SetBool ("play", false);
-					A_state = A_enemy_state.A_atk;//攻撃範囲内にプレイヤーがいたら攻撃実行
+                        A_anim.SetBool(A_animator_state[(int)A_state], false);
+                        A_state = A_enemy_state.A_atk;//攻撃範囲内にプレイヤーがいたら攻撃実行
 				} else {
-                        A_anim.SetBool("play", false);
-                        
+                        A_anim.SetBool(A_animator_state[(int)A_state], false);
                         A_state = A_enemy_state.A_vsb; //>プレイヤーが攻撃範囲外なら追いかける
 					
 				}
@@ -103,10 +88,7 @@ public class A_normal_enemy_move : Photon.MonoBehaviour {
       
 		case A_enemy_state.A_vsb://追いかけて来る状態(視認している)
 
-                /*移動のモーション*/
-			A_anim.SetBool ("run", true);
-                /****************/
-
+        
                 /******追尾*******/
 			transform.position = A_Unique_Move ();
                 /*****************/
@@ -114,19 +96,13 @@ public class A_normal_enemy_move : Photon.MonoBehaviour {
                 /*条件で攻撃(stateを攻撃に切り替え)*/
 			A_magnitude = (transform.position - A_Player.transform.position).magnitude;//二点間の距離
 			if (A_magnitude <= A_target_magnitude) {
-				A_anim.SetBool ("run", false);
-				A_state = A_enemy_state.A_atk;//プレイヤーと自分の距離が一定範囲以内なら攻撃へ
+                    A_anim.SetBool(A_animator_state[(int)A_state], false);
+                    A_state = A_enemy_state.A_atk;//プレイヤーと自分の距離が一定範囲以内なら攻撃へ
 			}
                 /********************************/
 			break;
 
 		case A_enemy_state.A_atk://攻撃
-			A_anim.SetBool ("run", false);
-			A_anim.SetBool ("play", false);
-                /*攻撃モーション*/
-			A_anim.SetBool ("attack", true);
-                /**************/
-
                 /*攻撃判定処理*/
 			A_Attack ();
                 /*************/
@@ -138,35 +114,31 @@ public class A_normal_enemy_move : Photon.MonoBehaviour {
 			A_hp -= A_B_info.Pow;
                 /************/
 			if (A_hp <= 0) {//体力が0(以下)なら状態を死亡に遷移
-				A_state = A_enemy_state.A_death;
+                    A_anim.SetBool(A_animator_state[(int)A_state], false);
+                    A_state = A_enemy_state.A_death;
 			} else {
-				/*ヒットモーション*/
+                    /*ヒットモーション*/
 
-				/****************/
-				/*まだ体力が余っているプレイヤーを追いかける*/
-				A_state = A_enemy_state.A_vsb;
+                    /****************/
+                    /*まだ体力が余っているプレイヤーを追いかける*/
+                    A_anim.SetBool(A_animator_state[(int)A_state], false);
+                    A_state = A_enemy_state.A_vsb;
 			}
                 /************/
 			break;
             
 		case A_enemy_state.A_death://死亡した状態(行動不能)		
-			A_anim.SetBool ("run", false);
-			A_anim.SetBool ("attack", false);
-			A_anim.SetBool ("play", false);
-			/***************/
-			if (A_anim.GetBool ("dide")) {
-				if (A_anim.GetCurrentAnimatorStateInfo (0).normalizedTime > 1.0f) { //0～1(始発から終点)
-					/*初期ポジションに戻る*/
-					/********************/
-				}else PhotonNetwork.Destroy (this.gameObject);//確認用
-			}
-
-                /*死亡モーション*/
-			A_anim.SetBool ("dide", true);
-			break;
+                if (A_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f)
+            { //0～1(始発から終点)
+                PhotonNetwork.Destroy(this.gameObject);//確認用
+            }
+                
+		    break;
 		}
+        
     }
 		
+
     /// <summary>
     /// 敵(ゾンビ)の行動
     /// </summary>
@@ -206,16 +178,16 @@ public class A_normal_enemy_move : Photon.MonoBehaviour {
                 /*プレイヤーダメージ処理*/
                 //A_P_info -= A_power; //プレイヤーのHPに自分の攻撃分減算する
                 //A_P_info.Damage(A_power);
-                Debug.Log("HIT!!");
+
                 A_delay_flg = false;    //ダメージが入った時にflgをfalse
                 /**********************/
             }
         }
         //アニメーションが終わったらflgをtrue
-        if (A_anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        if (A_anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f)
         {
             A_delay_flg = true;
-            A_anim.SetBool("attack", false);
+            A_anim.SetBool(A_animator_state[(int)A_state], false);
             /*攻撃モーションが終わったのでstateを切り替える(待機へ)*/
             A_state = A_enemy_state.A_oov;
             /**************************************************/
@@ -224,31 +196,39 @@ public class A_normal_enemy_move : Photon.MonoBehaviour {
         
     }
     
+
     /// <summary>
     /// 弾との当たり判定
     /// </summary>
     /// <param name="col"></param>
-    void OnCollisionEnter(Collision col)
+    protected void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name.StartsWith("Bullet"))
+        if (col.gameObject.name.StartsWith("Bullet") && A_state != A_enemy_state.A_death)
         {
             GameObject.Destroy(col.gameObject);
+            A_anim.SetBool(A_animator_state[(int)A_state], false);
             A_state = A_enemy_state.A_hit;
         }
     }
+    
 
     /// <summary>
-    /// instanceがtrueになった時に呼ばれる
+    /// プレイヤー参照（1P～4Pランダムで狙う）
     /// </summary>
-/*
-	IEnumerator Target(){
-		while(true){
-			A_player_target = Random.Range(1, 3);
-			if (A_player_target == 1) A_Player = GameObject.Find("Player1");
-			else if (A_player_target == 2) A_Player = GameObject.Find("Player2");
-			A_P_info = A_Player.GetComponent<N2_status> ();
-			yield return new WaitForSeconds (40f);
-		}
-	}*/
+    protected virtual void A_Player_Select()
+    {
+        do
+        {
+            A_Player = GameObject.Find("Player" + Random.Range(1, 5).ToString());//>1P～4Pをランダムで参照
+        } while (A_Player == null);//>プレイヤーが居たら抜け出す
+    }
 
+
+    /// <summary>
+    /// プレイヤー参照（1P～4Pどれを狙うか選ぶ）
+    /// </summary>
+    protected virtual void A_Player_Select(sbyte player_num)
+    {
+        A_Player = GameObject.Find("Player" + player_num.ToString());//>1P～4Pをランダムで参照   
+    }
 }
