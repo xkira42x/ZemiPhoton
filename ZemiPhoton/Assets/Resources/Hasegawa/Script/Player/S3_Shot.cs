2d23906b8b378bcf61afa4c79be7d490d090bcf8
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-12-18 10:06:55 +0900

    残弾数を表示するようにする

--- ZemiPhoton/Assets/Resources/Hasegawa/Script/Player/S3_Shot.cs
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/Player/S3_Shot.cs
@@ -16,11 +16,15 @@ public class S3_Shot : Photon.MonoBehaviour {
 
 	RaycastHit hitInfo;
 
+	Item_State2 iState;
+
 	void Start(){
 		if (MyGun != null)
 			MyGun.ShotSetting (shot);
 
 		shot = GetComponent<S3_Shot> ();
+
+		iState = GameObject.Find ("UI").GetComponent<Item_State2> ();
 	}
 
 	void Update () {
@@ -33,7 +37,7 @@ public class S3_Shot : Photon.MonoBehaviour {
 
 			if (Input.GetKeyDown (KeyCode.R)) {
 				MyGun.ReloadRequest ();
-                UI.text = "";
+				UI.text = "";
 			}
 
 			if (Physics.Raycast (CameraT.position, CameraT.forward, out hitInfo, 5, 1 << LayerMask.NameToLayer ("Item"))) {
@@ -42,6 +46,9 @@ public class S3_Shot : Photon.MonoBehaviour {
 					gameObject.SendMessage ("PickUpItemMSG", hitInfo.collider.gameObject, SendMessageOptions.DontRequireReceiver);
 				}
 			}
+
+			if (MyGun != null)
+				iState.Number_of_remaining_bullets = MyGun.GetMagazine ();
 		}
 	}
 
