f4f7ce2fca9e85ce856c7ec7ac2801c483d9f39d
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-12-24 22:27:02 +0900

    敵の追尾（ナビメッシュ使用）

---
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/Enemy/E_AI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class E_AI : Photon.MonoBehaviour {
+
+	[SerializeField]NavMeshAgent agent;
+	[SerializeField]Transform targetTransform;
+
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	void Update () {
+		if (targetTransform == null && PlayerList.length > 0) {
+			int ii = Random.Range (0, PlayerList.length);
+			photonView.RPC ("SyncTarget", PhotonTargets.AllBuffered, ii);
+		}
+
+		if (targetTransform != null) {
+			//if (agent.pathStatus != NavMeshPathStatus.PathInvalid)
+			agent.SetDestination (targetTransform.position);
+		}
+	}
+
+
+	[PunRPC]
+	void SyncTarget(int index){
+		targetTransform = PlayerList.GetPlayerList (index).transform;
+	}
+}
---
+++ ZemiPhoton/Assets/Resources/Hasegawa/Script/Enemy/E_NavControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class E_NavControl : MonoBehaviour {
+
+	[SerializeField]E_AI ai;
+	[SerializeField]NavMeshAgent agent;
+
+	void Start () {
+		StartCoroutine (SetNaviControl ());
+	}
+
+	IEnumerator SetNaviControl(){
+		yield return new WaitForSeconds (1);
+		agent.enabled = true;
+		ai.enabled = true;
+
+		Destroy (this);
+	}
+}
