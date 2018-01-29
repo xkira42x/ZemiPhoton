8a5a377cfb2cc3ee946ff4a9fb3d67119e732717
DA\nwuser <0ta5274738t4p4r@gmail.com>
2018-01-29 22:56:16 +0900

    スピッターのゲロが飛んでからゲロ溜ができるまでが完成

---
+++ ZemiPhoton/Assets/Resource/Hasegawa/Script/Enemy/E_GeroPuddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_GeroPuddle : MonoBehaviour {
+
+	void Start () {
+        Destroy(gameObject, 1f);
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter() { }
+}
