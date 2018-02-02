c56fe9648b3a5a4e6fed768354fc6f3b79ef0eeb
Amakitsune <6mikagura.ten9@gmail.com>
2018-02-02 11:18:22 +0900

    PlayerName Space  Complete

---
+++ ZemiPhoton/Assets/Resource/Shihara/Script/NameSpace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameSpace : MonoBehaviour {
+
+    public Text Name;
+    public S2_Status status;
+	// Use this for initialization
+	void Start () {
+        Name.text = status.UserName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Camera.main.transform.rotation;
+	}
+}
